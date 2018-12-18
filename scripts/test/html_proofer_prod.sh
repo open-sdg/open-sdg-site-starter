db514fe2e616273e703f94016513dc1fc3d2727c
Brock Fanning <brockfanning@gmail.com>
2018-12-18 09:01:11 -0500

    Revert "Trying shell settings to ensure Github receives status"
    This reverts commit 3bf1671d0aa8a51a37bfed0e6bf994feca57ed0e.

--- scripts/test/html_proofer_prod.sh
+++ scripts/test/html_proofer_prod.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Exit script if you try to use an uninitialized variable.
-set -o nounset
-# Exit script if a statement returns a non-true return value.
-set -o errexit
-# Use the error status of the first failure, rather than that of the last item in a pipeline.
-set -o pipefail
-
 # The following assumes the site is already built at _site.
 bundle exec htmlproofer --disable-external ./_site
--- scripts/test/html_proofer_staging.sh
+++ scripts/test/html_proofer_staging.sh
@@ -2,13 +2,6 @@
 
 # The following script assumes the site is already built at _site.
 
-# Exit script if you try to use an uninitialized variable.
-set -o nounset
-# Exit script if a statement returns a non-true return value.
-set -o errexit
-# Use the error status of the first failure, rather than that of the last item in a pipeline.
-set -o pipefail
-
 # Figure out the Jekyll baseurl. If this doesn't work for some reason, you can
 # just hardcode it, eg: JEKYLL_BASEURL="/my-baseurl"
 JEKYLL_BASEURL=$(
