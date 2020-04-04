4f2790b72e00efc150572df28e27410f2b6e4bcb
brockfanning <brockfanning@users.noreply.github.com>
2020-04-04 00:16:03 +0000

    Deploying to gh-pages from  @ e4f3270ef9317f77fe9b5663db2b290798e446c4 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -94,8 +94,8 @@ opensdg.autotrack = function(preset, category, action, label) {
     this.currentDisaggregation = 0;
 
     // Require at least one geoLayer.
-    if (!options.mapLayers.length) {
-      console.log('Map disabled, no mapLayers in options.');
+    if (!options.mapLayers || !options.mapLayers.length) {
+      console.log('Map disabled - please add "map_layers" in site configuration.');
       return;
     }
 
