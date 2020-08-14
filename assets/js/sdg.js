2b45ad11874b770676d480064946fb571c9542fd
brockfanning <brockfanning@users.noreply.github.com>
2020-08-14 00:48:38 +0000

    Deploying to gh-pages from  @ 174bf6334a28cc00110837e135d780363e71f003 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -590,6 +590,63 @@ Chart.plugins.register({
     }
   }
 });
+function getTextLinesOnCanvas(ctx, text, maxWidth) {
+  var words = text.split(" ");
+  var lines = [];
+  var currentLine = words[0];
+
+  for (var i = 1; i < words.length; i++) {
+      var word = words[i];
+      var width = ctx.measureText(currentLine + " " + word).width;
+      if (width < maxWidth) {
+          currentLine += " " + word;
+      } else {
+          lines.push(currentLine);
+          currentLine = word;
+      }
+  }
+  lines.push(currentLine);
+  return lines;
+}
+
+// This plugin displays a message to the user whenever a chart has no data.
+Chart.plugins.register({
+  afterDraw: function(chart) {
+    if (chart.data.datasets.length === 0) {
+
+      // @deprecated start
+      if (typeof translations.indicator.data_not_available === 'undefined') {
+        translations.indicator.data_not_available = 'This data is not available. Please choose alternative data to display.';
+      }
+      // @deprecated end
+
+      var ctx = chart.chart.ctx;
+      var width = chart.chart.width;
+      var height = chart.chart.height
+      chart.clear();
+
+      ctx.save();
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.font = "normal 40px 'Open Sans', Helvetica, Arial, sans-serif";
+      var lines = getTextLinesOnCanvas(ctx, translations.indicator.data_not_available, chart.chart.width);
+      var numLines = lines.length;
+      var lineHeight = 50;
+      var xLine = width / 2;
+      var yLine = (height / 2) - ((lineHeight / 2) * numLines);
+      for (var i = 0; i < numLines; i++) {
+        ctx.fillText(lines[i], xLine, yLine);
+        yLine += lineHeight;
+      }
+      ctx.restore();
+
+      $('#selectionsChart').addClass('chart-has-no-data');
+    }
+    else {
+      $('#selectionsChart').removeClass('chart-has-no-data');
+    }
+  }
+});
 function event(sender) {
   this._sender = sender;
   this._listeners = [];
@@ -2574,6 +2631,15 @@ var indicatorView = function (model, options) {
     }));
   }
 
+  this.tableHasData = function(table) {
+    for (var i = 0; i < table.data.length; i++) {
+      if (table.data[i].length > 1) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   this.createTable = function(table, indicatorId, el) {
 
     options = options || {};
@@ -2583,7 +2649,7 @@ var indicatorView = function (model, options) {
     // clear:
     $(el).html('');
 
-    if(table && table.data.length) {
+    if(table && this.tableHasData(table)) {
       var currentTable = $('<table />').attr({
         'class': table_class,
         'width': '100%'
@@ -2626,8 +2692,10 @@ var indicatorView = function (model, options) {
       // initialise data table
       initialiseDataTable(el);
 
+      $(el).removeClass('table-has-no-data');
     } else {
-      $(el).append($('<p />').text('There is no data for this breakdown.'));
+      $(el).append($('<h3 />').text(translations.indicator.data_not_available));
+      $(el).addClass('table-has-no-data');
     }
   };
 
