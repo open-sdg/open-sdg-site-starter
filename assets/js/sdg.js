83c1bfce4273ea9ba28ac5da40e39eef083dab07
brockfanning <brockfanning@gmail.com>
2021-09-17 15:33:37 +0000

    Deploying to gh-pages from  @ cca2f93ccf56450719cc68c5182f995724add638 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3136,7 +3136,7 @@ var indicatorView = function (model, options) {
     }));
 
     
-    if (units.length < 1) {
+    if (units.length <= 1) {
     
       $(this._rootElement).addClass('no-units');
     }
@@ -3155,7 +3155,7 @@ var indicatorView = function (model, options) {
       }));
 
       
-      if (serieses.length < 1) {
+      if (serieses.length <= 1) {
       
         $(this._rootElement).addClass('no-serieses');
       }
